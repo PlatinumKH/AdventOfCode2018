e3a046e2832d680ee31488cd7610e450b05469bc
Hak <hk14989@bristol.ac.uk>
2018-12-01 22:53:34 +0000

    Init

---
+++ Day1/Day1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+    	try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+	    	String line;
+	    	int total = 0;
+	    	while ((line = br.readLine()) != null) {
+	       		total = total + Integer.parseInt(line);
+    		}
+    		System.out.println(total);
+		} catch (Exception e){
+			System.out.println(e);
+		}
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+My attempt at the 2018 Advent Of Code: https://adventofcode.com/2018
