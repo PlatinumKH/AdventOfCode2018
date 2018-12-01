e11178a7d7b7fdd3533d4847fd9649f49ec0da81
Hak <hk14989@bristol.ac.uk>
2018-12-01 22:59:38 +0000

    Init and Day1Part1 completed

--- Day1/Part1.java
+++ Day1/Part1.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class Part1 {
+public class Day1 {
 
     public static void main(String[] args) {
     	try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
